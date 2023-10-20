3d659dc3355a3b7bcf9df523566952ed6397458f
Silvio Ukoroo <105038521+silvioukoth@users.noreply.github.com>
2023-10-20 23:37:09 +0300

    add my commits

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
